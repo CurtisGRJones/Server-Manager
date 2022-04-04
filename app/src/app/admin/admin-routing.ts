ba939fbcb0c6784f7df5ce00c3f26ac624f56f35
Curtis Jones <jonesc21@mcmaster.ca>
2022-04-04 19:04:08 -0400

    made a bunch of components

--- app/src/app/admin/admin-routing.ts
+++ app/src/app/admin/admin-routing.ts
@@ -1,8 +1,34 @@
 import {Routes} from "@angular/router";
+import {UserUsageComponent} from "@app/admin/user-usage/user-usage.component";
+import {ServerUsageComponent} from "@app/admin/server-usage/server-usage.component";
+import {RegistrationRequestsComponent} from "@app/admin/registration-requests/registration-requests.component";
+import {AddGameComponent} from "@app/admin/add-game/add-game.component";
+import {AddServerComponent} from "@app/admin/add-server/add-server.component";
+import {ManageUsersComponent} from "@app/admin/manage-users/manage-users.component";
 
 export const adminRoutes: Routes = [
   {
-    path: "admin/test",
-    redirectTo: 'admin',
+    path: "admin/userUsage",
+    component: UserUsageComponent,
+  },
+  {
+    path: "admin/serverUsage",
+    component: ServerUsageComponent,
+  },
+  {
+    path: "admin/pendingRegistations",
+    component: RegistrationRequestsComponent,
+  },
+  {
+    path: "admin/addGame",
+    component: AddGameComponent,
+  },
+  {
+    path: "admin/addServer",
+    component: AddServerComponent,
+  },
+  {
+    path: "admin/users",
+    component: ManageUsersComponent,
   },
 ]
